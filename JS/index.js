ff31b2967696c1d5fec5edee473e4adbcf4f7ff5
bgroyer1 <royerbraedan@gmail.com>
2024-04-01 15:17:10 -0700

    Refactored code
    redid the assignment to complete code

--- JS/index.js
+++ JS/index.js
@@ -1,36 +1,33 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
-const generateBtn = document.querySelector("#generate-btn");
-const password1Div = document.querySelector("#password-div1");
-const password2Div = document.querySelector("#password-div2");
-const characterLimit = document.querySelector("#character-limit")
-const limitSubmit = document.querySelector("#character-limit-submit") 
-const p1El = document.querySelector("#p-1")
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
+let count = characters.length;
+let password1 = ""
+let password2 = ""
+const password1El = document.querySelector(".password1-el")
+const password2El = document.querySelector(".password2-el")
+const passwordBtn = document.querySelector(".btn-password-generator")
 
 function randomCharacter() {
-    return characters[Math.floor(Math.random() * characters.length)]
+    let randomCharacter = Math.floor(Math.random() * 91)
+    return characters[randomCharacter]
+    console.log(characters[randomCharacter])
 }
 
-
-function randomPassword() {
+function generatePassword() {
     let password = ""
-    for (let i = 0; i < characters.length; i++) {
-        if (password.length < 15) {
-            password += randomCharacter()
-        } else {
-            break;
-        }
+    for (let i = 0; i < 16; i ++) {
+        password += randomCharacter()
+        console.log(password)
     }
     return password
 }
 
-generateBtn.addEventListener("click", () => {
-    let password1 = randomPassword()
-    let password2 = randomPassword();
-    password1Div.textContent = password1
-    password2Div.textContent = password2
-})
+passwordBtn.addEventListener("click", () => {
+    password1El.textContent = generatePassword()
+    password2El.textContent = generatePassword()
+}
+)
 
 document.getElementById("clipboard1").addEventListener("click", () => {
     var range = document.createRange();
